46c9f9248e30ae28d1d79a210bede0ea44b18939
reawphongsaphak <reawza00789@gmail.com>
2024-07-07 23:25:24 +0700

    week2

--- week2/integrate.c
+++ week2/integrate.c
@@ -1,42 +1,37 @@
-#include <stdio.h>
 #include <math.h>
 #ifndef M_PI
-#define M_PI 3.141592653589793238462643383279502884
+#define M_PI 3.14159265358979323846
 #endif
 
-float cal_area(float d, float h1, float h2);
-float graph_h(float A, float B, float x);
+double cal_area(double d, double h1, double h2);
+double graph_h(double A, double B, double x);
 
 int main(){
-  float a, b, A, B, d, ans = 0.0;
-  int  n;
+    double a, b, A, B, d, ans = 0.0;
+    int  n;
 
-  scanf("%f %f %f %f %d", &a, &b, &A, &B, &n);
-  d = (b - a) / n;
-  for (int i = 0; i < n; i++){
+    scanf("%lf %lf %lf %lf %d", &a, &b, &A, &B, &n);
+    d = (b - a) / n;
 
-    float x1 = a +  i * d;
-    float x2 = a +  (i + 1) * d;
+    for (int i = 0; i < n; i++){
+        double x1 = a +  i * d;
+        double x2 = a +  (i + 1) * d;
 
-    float h1 = graph_h(A, B, x1);
-    float h2 = graph_h(A, B, x2);
-    float area = (float)cal_area(d, h1, h2);
+        double h1 = graph_h(A, B, x1);
+        double h2 = graph_h(A, B, x2);
+        double area = (double)cal_area(d, h1, h2);
 
-   // printf("area is : %.5lf\n",area);
-   // printf("h1 is : %.5lf\n",h1);
-   // printf("h2 is : %.5lf\n",h2);
-    ans += area;
-  }
-  
-  printf("%.5lf\n", ans);
-  
-  return 0;
+        ans += area;
+    }
+    printf("%.5lf\n", ans);
+
+    return 0;
 }
 
-float cal_area(float d, float h1, float h2){
-  return 0.5 * d * ( h1 + h2);
+double cal_area(double d, double h1, double h2){
+    return 0.5 * d * ( h1 + h2);
 }
 
-float graph_h(float A, float B, float x){
-  return A * sin( (M_PI*x) / B);
+double graph_h(double A, double B, double x){
+    return A * sin( (M_PI*x) / B);
 }
